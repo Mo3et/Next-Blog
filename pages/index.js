3912efabc0cdc65f9070888c1899b90d989a9493
Mo3et <monet_lee@163.com>
2021-05-11 23:06:55 +0800

    add new blog

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-
+// export default Home
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+       <title>Create React App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
